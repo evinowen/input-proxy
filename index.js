22f26d17265f445b788b3837624e7ac26cdafd2d
Evin Owen <eowen@launchcg.com>
2020-04-07 13:39:29 -0700

    Remove unused countdown function

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function countdown (seconds) {
-  for (let i = seconds; i > 0; i--) {
-    console.log('countdown >>', i)
-    await wait(SECOND)
-  }
-}
-
 function load (path) {
   return new Promise((resolve, reject) =>
     fs.readFile(path, 'utf8', (error, data) => error ? reject(error) : resolve(JSON.parse(data)))
